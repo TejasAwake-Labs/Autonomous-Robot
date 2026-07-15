7bd44d56af8d5042a033903593640d9e458d41e1
arhaan-pawaskar <arhaanpawaskar817@gmail.com>
2026-07-15 21:37:22 +0530

    Adding template code

---
+++ template.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+list_of_files=[
+    "requirements.txt"
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    
+    else:
+        logging.info(f"{filename} is already exists")
